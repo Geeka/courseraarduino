405db3c55c2cfe33d6c4c548fe8486601c16e3ad
Geeka <Geeka@users.noreply.github.com>
2020-01-22 11:52:59 +0530

    Eeprom serial monitor

--- Eepromsketch.ino
+++ Eepromsketch.ino
@@ -1,31 +1,87 @@
+#include <EEPROM.h>;
 
-#include <EEPROM.h>
+int memsize = EEPROM.length();
+int valsize = 255;
+
 void setup() 
 {
   Serial.begin(9600);
 }
-int address;
-int dataToWrite;
+int addr,value;
+String cmd;
 
 void loop() 
 {
+  if(Serial.available() ){
   String buffer = "";
   buffer = Serial.readString();
-  if(buffer.startsWith("read")) {
-    address = buffer.substring(buffer.indexOf(' ') +1).toInt();
+  Serial.println(buffer);
+    
+  int cmdi = splitString(buffer);
+  if (cmdi==1)
+    {
     Serial.print("Data from the Address : ");
-    Serial.print(address);
+    Serial.print(addr);
     Serial.println();
-    Serial.println(EEPROM.read(address)); 
-  } else if(buffer.startsWith("write")) {
-    address = buffer.substring(6,7).toInt();
-    dataToWrite = buffer.substring(8).toInt();
+    Serial.println(EEPROM.read(addr)); 
+    } 
+  else if(cmdi==2) {
     Serial.print("Written ");
-    Serial.print(dataToWrite);
+    Serial.print(value);
     Serial.println();
     Serial.print("to the Address: ");
-    Serial.print(address);
+    Serial.print(addr);
     Serial.println();
-    EEPROM.write(address,dataToWrite); 
+    EEPROM.write(addr,value);
+    }
+   }
+  
+}
+
+int splitString(String str) {
+    
+  String result[3]={"","",""};
+  int r=0;
+  
+  for(int  i=0; i <3; i++){
+    int index= str.indexOf(' ');
+    result[i] = str.substring(0,index);
+    str= str.substring(index+1);
   }
+  
+  cmd=result[0];
+  addr=result[1].toInt();
+  value = result[2].toInt();
+  
+  if(cmd.equals("read")) r=1;
+  else if(cmd.equals("write")) r=2;
+  
+          
+  if (!r)
+  {
+      Serial.println("invalid command");
+      return -1;
+  }
+  
+    
+  
+  if(!isNumeric( result[1]) || addr >= memsize) {
+      Serial.println("invalid addresd");
+      return -2;
+   }
+  
+  
+  if((!isNumeric(result[2]) || value>255) && r==2) {
+        Serial.println ("invalid data to write");
+        return -3;
+  }
+          
+  return r;
+}
+          
+bool isNumeric(String str) {
+  for( int i=0;i<str.length();i++) {
+    if (!isDigit(str[i])) return 0;
+  }
+  return 1;
 }
